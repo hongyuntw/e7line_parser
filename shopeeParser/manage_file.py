79471e02d0382d1547b2230723fa0331d89b019b
HongYun0901 <harrychiang0@gmail.com>
2020-08-31 11:20:08 +0800

    keep log file

--- shopeeParser/manage_file.py
+++ shopeeParser/manage_file.py
@@ -7,7 +7,6 @@ yesterday = datetime.strftime(datetime.now() - timedelta(1), "%m_%d_%Y")
 
 
 for store_name in ['momo', 'shopee','yahoo']:
-
     base_path = './' + store_name
     for root, dirs, files in os.walk(base_path):
         for dir in dirs:
@@ -19,14 +18,14 @@ for store_name in ['momo', 'shopee','yahoo']:
                 print(folder_path + ' deleted')
             except:
                 print(folder_path + ' delete failed')
-    log_path  = './' + store_name + '_log.txt'
-    if os.path.exists(log_path):
-        os.remove(log_path)
-    else:
-        print(store_name + " log does not exist")
-
-    file = open(log_path, "w") 
-    file.close() 
+    
+    # log_path  = './' + store_name + '_log.txt'
+    # if os.path.exists(log_path):
+    #     os.remove(log_path)
+    # else:
+    #     print(store_name + " log does not exist")
+    # file = open(log_path, "w") 
+    # file.close() 
 
 
 
